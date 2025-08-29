f1169dbe2782d8372ed7492723abc7b72113d211
Xseis <casper.scarth.tonseth@gmail.com>
2025-08-29 20:00:01 +0200

    aa

--- resources/js/tabmanager.js
+++ resources/js/tabmanager.js
@@ -1,32 +1,38 @@
 const mainTab = document.getElementById("mainTab");
 const brukerstotteTab = document.getElementById("brukerstotteTab");
+const yffTab = document.getElementById("yffTab");
 const tabs = [
     mainTab,
-    brukerstotteTab
-]
+    brukerstotteTab,
+    yffTab
+];
 
-const mainContent = document.getElementById("mainContent")
-const BSU3Content = document.getElementById("BSU3Content")
-const BSU35Content = document.getElementById("BSU35Content")
+const mainContent = document.getElementById("mainContent");
+const BSU3Content = document.getElementById("BSU3Content");
+const BSU33Content = document.getElementById("BSU33Content");
+const BSU34Content = document.getElementById("BSU34Content");
+const BSU35Content = document.getElementById("BSU35Content");
 const contents = [
     mainContent,
     BSU3Content,
+    BSU33Content,
+    BSU34Content,
     BSU35Content
-]
+];
 
 function gotoTab(tabid) {
     hideTab()
     const tab = document.getElementById(tabid);
     tab.style.display = "block";
     if (tabid == "mainTab") {
-        gotoContent("mainContent")
+        gotoContent("mainContent");
     } else if (tabid == "brukerstotteTab") {
-        gotoContent("brukerstotteContent")
+        gotoContent("brukerstotteContent");
     }
 }
 
 function gotoContent(contentid) {
-    hideContent()
+    hideContent();
     document.getElementById(contentid).style.display = "block";
 }
 
